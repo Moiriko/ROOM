26f5904cfb775512a60fd6a4a3d198fcc5471ac9
Moiriko <mairam.askarova8@gmail.com>
2021-04-02 16:13:55 +0600

    Added new project

---
+++ src/js/brands.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.brands__swiper-container', {
+    loop:true,
+    autoplay: true,
+    centeredSlides: true,
+    speed: 100,
+    spaceBetween: 1,
+    slidesPerView:1,
+    slidesPerGroup:1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
---
+++ src/js/work.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.work__swiper-container', {
+    loop:true,
+    autoplay: true,
+    centeredSlides: true,
+    speed: 100,
+    spaceBetween: 0,
+    slidesPerView:1,
+    slidesPerGroup:1,
+  });
